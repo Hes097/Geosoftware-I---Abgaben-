b9c91bb0d95ed99a849876573073345777259954
Hes097 <henning097@live.com>
2021-06-07 11:47:11 +0200

    Different changes

---
+++ database_template.js
@@ -0,0 +1,97 @@
+// template to add to and retrieve documents from a MongoDB database
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert') //Prüft ob zwei Werte gleich sind
+
+const url = 'mongodb://localhost:27017' // connection URL
+
+const client = new MongoClient(url) // mongodb client
+
+const dbName = 'mydb' // database name
+
+const collectionName = 'test' // collection name
+
+// Use connect method to connect to the server
+client.connect(function(err) 
+{
+  assert.equal(null, err)
+
+  console.log('Connected successfully to server')
+
+  const db = client.db(dbName)
+  /*
+    // create a collection 
+    db.createCollection(collectionName, function(err, res) 
+    {
+        if (err) throw err
+        console.log("Collection created!")
+    })
+  */
+  const collection = db.collection(collectionName)
+
+  const data = [
+    {"type": "FeatureCollection", 
+  "features:": [
+    {"type": "Feature", 
+      "geometry": {"type": "Point", "coordinates": [7.628056, 51.96222]}, 
+      "properties": {
+        "cityname": "Münster"}}
+  ]}
+  ]
+
+  // insert new documents in my collection
+  insertDocuments(db, collection, data, function()
+  {
+    console.log("Insertion operation done")
+    //client.close()
+  })
+
+  countRows (collection)
+
+  findDocuments(db, collection, function()
+  {
+    console.log("Closing the client...")
+    client.close()
+  })
+
+})
+
+
+const insertDocuments = function(db, collection, data, callback) 
+{
+    // Insert some documents
+    collection.insertMany(data, function(err, result) 
+    {
+      assert.equal(err, null)
+      assert.equal(1, result.result.ok)
+      //console.log(result)
+      console.log(`Inserted ${result.insertedCount} documents into the collection`)
+      callback(result)
+    })
+}
+
+// find all documents in a collection
+const findDocuments = function(db, collection, callback) 
+{
+
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) 
+    {
+      assert.equal(err, null);
+      console.log('Found the following records');
+      console.log(docs);
+      callback(docs)
+    })
+}
+
+// count the number of rows in a collection
+const countRows = function(collection)
+{
+    // count the total number of elements in my collection
+    collection.count(function (err, count) 
+    {
+        if (err) throw err
+    
+        console.log('Total Rows: ' + count)
+    })
+
+}
--- routes/2_add.js
+++ routes/2_add.js
@@ -1,9 +1,50 @@
 var express = require('express');
 var router = express.Router();
+const MongoClient = require('mongodb').MongoClient
+const assert = require('assert')
+
+const url = 'mongodb://localhost:27017' // connection URL
+const client = new MongoClient(url) // mongodb client
+const dbName = 'mydb' // database name --> muss noch geändert werden
+const collectionName = 'newPersons' // collection name --> muss noch geändert werden 
 
 /* GET add page. */
 router.get('/', function(req, res, next) {
-  res.render('2_add', { title: 'Add' });
+  res.render('2_add', { title: 'Add new route to the database' });
 });
+
+//Insert new data
+router.get('/', function(req, res, next) 
+{
+  console.log("A new person has been added")
+  console.log(req.body)
+  let person = {}
+  person.name = req.body.pname
+  person.address = req.body.cname 
+
+
+  //Connect to the mongodb database afterwards, insert one new element 
+  client.connect(function(err)
+  {
+    assert.equal(null, err)
+
+    console.log('Connected successfully to server')
+
+    const db = client.db(dbName)
+    const collection = db.collection(collectionName)
+    //Insert the document in the database 
+    collection.insertOne(person, function(err, results)
+    {
+      assert.equal(err, null)
+      assert.equal(1, result.result.ok)
+      //console.log(result)
+      console.log(`Inserted ${result.insertedCount} document intp the databse`)
+      res.render('2_add_notification', {title: 'Addition completed', data: person})
+    })
+
+  })
+
+
+})
 
 module.exports = router;
